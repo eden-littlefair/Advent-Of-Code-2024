24defdcfe63d022d041bd0a8102812b5eb4b2d1e
eden-littlefair <18LittlefairE@sphcs.co.uk>
2024-12-12 17:07:02 +0000

    Add files via upload

--- day4/day4p2.py
+++ day4/day4p2.py
@@ -17,37 +17,32 @@ for i in range(len(arr)-2):
         if arr[i][j] == "M" and arr[i][j+2] == "M" and arr[i+1][j+1] == "A" and arr[i+2][j] == "S" and arr[i+2][j+2] == "S":
             xmasCountp2 += 1
 
+#for ["S", "", "S"
+#     "", "A", ""
+#     "M", "", "M"]
+
 for i in range(len(arr)-2): 
     for j in range(len(arr)-2):
         if arr[i][j] == "S" and arr[i][j+2] == "S" and arr[i+1][j+1] == "A" and arr[i+2][j] == "M" and arr[i+2][j+2] == "M":
             xmasCountp2 += 1
+
+#for ["S", "", "M"
+#     "", "A", ""
+#     "S", "", "M"]
 
 for i in range(len(arr)-2): 
     for j in range(len(arr)-2):
         if arr[i][j] == "S" and arr[i][j+2] == "M" and arr[i+1][j+1] == "A" and arr[i+2][j] == "S" and arr[i+2][j+2] == "M":
             xmasCountp2 += 1
 
+#for ["M", "", "S"
+#     "", "A", ""
+#     "M", "", "S"]
+
 for i in range(len(arr)-2): 
     for j in range(len(arr)-2):
         if arr[i][j] == "M" and arr[i][j+2] == "S" and arr[i+1][j+1] == "A" and arr[i+2][j] == "M" and arr[i+2][j+2] == "S":
             xmasCountp2 += 1
-
-'''
-for i in range(len(arr[0])-3): #for rotation south-east
-    for j in range(len(arr)-3):
-        if arr[i][j] == "X" and arr[i+1][j+1] == "M" and arr[i+2][j+2] == "A" and arr[i+3][j+3] == "S":
-            xmasCount += 1
-
-for i in range(len(arr[0])-3): #for rotation south
-    for j in range(len(arr)):
-        if arr[i][j] == "X" and arr[i+1][j] == "M" and arr[i+2][j] == "A" and arr[i+3][j] == "S":
-            xmasCount += 1
-
-for i in range(0, len(arr[0])-3): #for rotation south-west
-    for j in range(3, len(arr)):
-        if arr[i][j] == "X" and arr[i+1][j-1] == "M" and arr[i+2][j-2] == "A" and arr[i+3][j-3] == "S":
-            xmasCount += 1
-'''
 
 print("XMAS count part 2:",xmasCountp2)
 #print(arr) 
